351430eda780062b2353ec7b9bac4f04cbc61e9b
KovaletsV <kovalets2902@gmail.com>
2022-12-13 19:25:49 -0700

    done

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./style";
+import {
+    Navbar,
+    Testimonials,
+    Clients,
+    Business,
+    CTA,
+    Hero,
+    Billing,
+    CardDeal,
+    Stats,
+    Footer
+} from "./components/index";
+
+const App = () => (
+    <div>
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+                <Navbar />
+            </div>
+        </div>
+
+        <div className={`bg-primary ${styles.flexStart}`}>
+            <div className={`${styles.boxWidth}`}>
+                <Hero />
+            </div>
+        </div>
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+                <Stats />
+                <Business />
+                <Billing />
+                <CardDeal />
+                <Testimonials />
+                <Clients />
+                <CTA />
+                <Footer />
+            </div>
+        </div>
+    </div>
+);
+
+export default App;
